07624919aaf3e3afda6fc5c42d961980025a4e82
Rahul Raj <118061407+rahul-raj9367@users.noreply.github.com>
2023-05-16 04:59:00 +0530

    Update db.js

--- Delete documents from the collection/db.js
+++ Delete documents from the collection/db.js
@@ -1,6 +1,5 @@
 //Delete documents from the collection
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const { connect, connection } = require('mongoose');
 const uri = "mongodb+srv://Rahulraj:Rahulraj-2002@cluster0.dh2u9rs.mongodb.net/?retryWrites=true&w=majority";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
