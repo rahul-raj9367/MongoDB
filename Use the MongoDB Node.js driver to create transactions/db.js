354fcd3fa3b5411e8595979e56c5336103f65399
Rahul Raj <118061407+rahul-raj9367@users.noreply.github.com>
2023-05-16 05:16:56 +0530

    Update db.js

--- Use the MongoDB Node.js driver to create transactions/db.js
+++ Use the MongoDB Node.js driver to create transactions/db.js
@@ -2,9 +2,8 @@ const mongodb=require('mongodb');
 const MongoClient=mongodb.MongoClient;
 const ObjectID=mongodb.ObjectId;
 let database;
-
 async function getDatabase(){
-    const client=await MongoClient.connect("mongodb://127.0.0.1:27017");
+    const client=await MongoClient.connect("mongodb+srv://Rahulraj:Rahulraj-2002@cluster0.dh2u9rs.mongodb.net/?retryWrites=true&w=majority");
     database=client.db('library');
     if(!database){
         console.log('Data Base Not connected');
